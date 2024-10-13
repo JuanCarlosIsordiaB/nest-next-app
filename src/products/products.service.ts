30ae8751c01f4073dc60840a625d708b7f5c2982
JuanCarlosIsordiaB <juancarlosisordiab@gmail.com>
2024-10-13 16:26:21 -0600

    Swagger

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,49 +1,68 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, ConflictException } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class ProductsService {
-
-
   constructor(private prisma: PrismaService) {}
 
-  create(createProductDto: CreateProductDto) {
-
-    return this.prisma.product.create({
-      data: createProductDto
-    });
-  }
-
-  findAll() {
-    return this.prisma.product.findMany();
-  }
-
-  findOne(id: string) {
-    const productFound = this.prisma.product.findUnique({where: {id}});
-    if(!productFound){
-      throw new Error('Product not found');
+  async create(createProductDto: CreateProductDto) {
+    try {
+      return await this.prisma.product.create({
+        data: createProductDto,
+      });
+    } catch (error) {
+      if(error.code === 'P2002')throw new ConflictException('This product already exists');
+      throw new Error(error);
+      
     }
-    return productFound;
   }
 
-  update(id: string, updateProductDto: UpdateProductDto) {
-    const productFound = this.prisma.product.findUnique({where: {id}});
-    if(!productFound){
-      throw new Error('Product not found');
+  async findAll() {
+    try {
+      return await this.prisma.product.findMany();
+    } catch (error) {
+      throw new NotFoundException('Products not found');
     }
-    return this.prisma.product.update({
-      where: {id},
-      data: updateProductDto
-    });
   }
 
-  remove(id: string) {
-    const productFound = this.prisma.product.findUnique({where: {id}});
-    if(!productFound){
-      throw new Error('Product not found');
+  async findOne(id: string) {
+    try {
+      const productFound = this.prisma.product.findUnique({ where: { id } });
+      if (!productFound) {
+        throw new NotFoundException('Products not found');
+      }
+      return await productFound;
+    } catch (error) {
+      throw new Error(error);
     }
-    return this.prisma.product.delete({where: {id}});
+  }
+
+  async update(id: string, updateProductDto: UpdateProductDto) {
+    try {
+      const productFound = this.prisma.product.findUnique({ where: { id } });
+      if (!productFound) {
+        throw new NotFoundException('Products not found');
+      }
+      return await this.prisma.product.update({
+        where: { id },
+        data: updateProductDto,
+      });
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  async remove(id: string) {
+    try {
+      const productFound = this.prisma.product.findUnique({ where: { id } });
+      if (!productFound) {
+        throw new NotFoundException('Products not found');
+      }
+      return await this.prisma.product.delete({ where: { id } });
+    } catch (error) {
+      throw new Error(error);
+    }
   }
 }
